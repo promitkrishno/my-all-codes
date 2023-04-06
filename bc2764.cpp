e4bedbcd67592dca7e4a2aba82879f7ed4249610
promitkrishno <124575065+promitkrishno@users.noreply.github.com>
2023-04-06 12:19:02 +0600

    Add files via upload

--- bc2764.cpp
+++ bc2764.cpp
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int main() {
+    char date[8];
+    int day, month, year;
+
+    while(scanf("%s", date) == 1) {
+        // Convert date string to integers
+        sscanf(date, "%d/%d/%d", &day, &month, &year);
+
+        // Print date in MM/DD/YY format
+        printf("%02d/%02d/%02d\n", month, day, year);
+
+        // Print date in YY/MM/DD format
+        printf("%02d/%02d/%02d\n", year, month, day);
+
+        // Print date in DD-MM-YY format
+        printf("%02d-%02d-%02d\n", day, month, year);
+    }
+
+    return 0;
+}
